c9db3844d4f6cd1e50d416d751a3a46bca0ca918
Kai Magalhães <magalhaes8500@gmail.com>
2026-04-10 10:04:19 +0100

    x

--- app-create.js
+++ app-create.js
@@ -1,23 +1,34 @@
 // 1. Importações das bibliotecas específicas
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} 
+from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+
 
 // 2. Configuração do projeto Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAzEUAvgalAp7CaWL789aFO8WP9heR6hXM",
-    authDomain: "teste-firebase-f4e71.firebaseapp.com",
-    projectId: "teste-firebase-f4e71",
-    storageBucket: "teste-firebase-f4e71.firebasestorage.app",
-    messagingSenderId: "914804237483",
-    appId: "1:914804237483:web:f019d2bf3f0f57b47703ee"
+  apiKey: "AIzaSyByK7sas0s_vJRVsogSKkzimOYH-oKEAhE",
+  authDomain: "batata-69.firebaseapp.com",
+  projectId: "batata-69",
+  storageBucket: "batata-69.firebasestorage.app",
+  messagingSenderId: "1041017537298",
+  appId: "1:1041017537298:web:b9d7ff1db5f983ef1569cb",
+  measurementId: "G-KRCYY9LWNG",
 };
 
-// 3. Inicialização dos Serviços
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
+const bd = getFirestore(app);
 
 // 4. Escuta do Evento de Submissão
-document.getElementById("formFuncionario").addEventListener("submit", async (e) => {
+document
+  .getElementById("formFuncionario")
+  .addEventListener("submit", async (e) => {
     e.preventDefault(); // Bloqueia o refresh da página
 
     const btn = document.getElementById("btnGravar");
@@ -26,26 +37,26 @@ document.getElementById("formFuncionario").addEventListener("submit", async (e) 
 
     // 5. Mapeamento dos dados para o formato NoSQL
     const novoFuncionario = {
-        nome: document.getElementById("nome").value,
-        morada: document.getElementById("morada").value,
-        contacto: {
-            email: document.getElementById("email").value,
-            telemovelPessoal: document.getElementById("telemovelPessoal").value,
-            telefoneFixo: document.getElementById("telefoneFixo").value,
-            telemovelTrabalho: document.getElementById("telemovelTrabalho").value
-        },
-        dataRegisto: new Date()
+      nome: document.getElementById("nome").value,
+      morada: document.getElementById("morada").value,
+      contacto: {
+        email: document.getElementById("email").value,
+        telemovelPessoal: document.getElementById("telemovelPessoal").value,
+        telefoneFixo: document.getElementById("telefoneFixo").value,
+        telemovelTrabalho: document.getElementById("telemovelTrabalho").value,
+      },
+      dataRegisto: new Date(),
     };
 
     try {
-        // 6. Envio para a Coleção "funcionarios"
-        const docRef = await addDoc(collection(db, "funcionarios"), novoFuncionario);
-        alert("Sucesso! Funcionário registado com o ID: " + docRef.id);
-        window.location.href = "index.html"; // Redireciona para a lista
+      // 6. Envio para a Coleção "funcionarios"
+      const docRef = await addDoc(collection(bd, "funcion."), novoFuncionario);
+      alert("Sucesso! Funcionário registado com o ID: " + docRef.id);
+      window.location.href = "index.html"; // Redireciona para a lista
     } catch (error) {
-        console.error("Erro ao gravar:", error);
-        alert("Erro ao guardar dados. Verifica a consola.");
-        btn.disabled = false;
-        btn.innerText = "Gravar na Cloud";
+      console.error("Erro ao gravar:", error);
+      alert("Erro ao guardar dados. Verifica a consola.");
+      btn.disabled = false;
+      btn.innerText = "Gravar na Cloud";
     }
-});
+  });
